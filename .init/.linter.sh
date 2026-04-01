84e0204d1020563695a607ef86c5aabdf70f1b58
Kavia AI <production@kavia.ai>
2026-04-01 04:50:19 +0000

    Checkpoint 1 - cge485bc1c

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/notesync-pro-338139-338155/notes_frontend
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
